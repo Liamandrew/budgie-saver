b6fa498f8c763fb3e827813664ccbb56736925c9
Liamandrew <liam.e.andrew@gmail.com>
2018-05-02 21:02:32 +1000

    Migration from private repo

---
+++ ios/Pods/Target Support Files/Pods-BudgieSaver-tvOS/Pods-BudgieSaver-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BudgieSaver_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_BudgieSaver_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-BudgieSaver-tvOSTests/Pods-BudgieSaver-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BudgieSaver_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_BudgieSaver_tvOSTests
+@end
